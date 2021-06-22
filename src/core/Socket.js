5cddf998332f0f02ba37279c09d78965118e6a45
valdofal <v.dofal@gmail.com>
2021-06-22 18:17:16 +0200

    added dot env for herok URL

--- src/core/Socket.js
+++ src/core/Socket.js
@@ -1,5 +1,6 @@
 import { createContext } from 'react'
 import {io} from "socket.io-client"
+import {URL} from '@env'
 
-export const socket = io.connect('https://cross-pwgame.herokuapp.com');
+export const socket = io.connect(URL || 'http://localhost:3000');
 export const SocketContext = createContext();
