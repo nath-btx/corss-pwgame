a421f729a05eca01574f30af9a3cd9f6bd732ace
valdofal <v.dofal@gmail.com>
2021-06-29 11:34:19 +0200

    final push

--- src/core/Socket.js
+++ src/core/Socket.js
@@ -1,6 +1,5 @@
 import { createContext } from 'react'
 import {io} from "socket.io-client"
-// import {URL} from '@env'
 
-export const socket = io.connect('http://192.168.1.112:3000');
+export const socket = io.connect('https://cross-pwgame.herokuapp.com');
 export const SocketContext = createContext();
